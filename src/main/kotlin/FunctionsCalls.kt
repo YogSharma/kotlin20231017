54f3c376bb11d417f852ec6384e4b1b9cee738db
YogSharma <amrajansh2053@gmail.com>
2023-10-18 19:53:34 +0545

    made some changes in function call file. there were some errors

--- src/main/kotlin/FunctionsCalls.kt
+++ src/main/kotlin/FunctionsCalls.kt
@@ -1,17 +1,58 @@
-fun main() {
-    val dog = Dog(4, "Sam")
-    println(dog.getInfo())
-    dog.incrementAge()
-    println(dog.getInfo())
-    println(dog.name) // getter called in the background
-    println()
+class Functions {
+    companion object {
+        var name: String = "default name"
+    }
 
+    fun printString(message: String) {
+        println(message)
+    }
+
+    fun printInfo() {
+        println("Name: $name")
+    }
+
+    fun saveTextToFile(content: String, filename: String = "default.txt") {
+        // Code to save content to a file with the specified or default filename
+    }
+}
+
+fun main() {
+    // Create a Dog instance with 4 legs and the name "Sam"
+    val dog = Dog(4, "Sam")
+
+    // Print initial information about the dog
+    println("Initial Dog Info:")
+    println(dog.getInfo())
+
+    // Increment the dog's age
+    dog.incrementAge()
+
+    // Print updated information about the dog
+    println("\nUpdated Dog Info:")
+    println(dog.getInfo())
+
+    // Access the 'name' property using the getter
+    println("Dog's Name: ${dog.name}")
+
+    // Create an instance of the Functions class
     val functions = Functions()
+
+    // Call the printString method with a message
     functions.printString("Hello world!")
 
-    Functions.name = "other name" // setter called in the background
+    // Set the 'name' property of the Functions class using the setter
+    Functions.name = "other name"
+
+    // Print information from the Functions class
     functions.printInfo()
-    functions.saveTextToFile("file content", "other filename.txt")
-    functions.saveTextToFile("file content") // using default filename
-    functions.saveTextToFile(content = "another content") // specifically using parameter name
+
+    // Save text to a file with a specific filename
+    functions.saveTextToFile("file content", "other_filename.txt")
+
+    // Save text to a file using the default filename
+    functions.saveTextToFile("file content")
+
+    // Save text to a file with a specific content and parameter name
+    functions.saveTextToFile(content = "another content")
 }
+
